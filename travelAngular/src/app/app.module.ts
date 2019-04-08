d400aea0f33dd2b9a70646ce3c975aa692b1d306
MarkLozier <lozierm2@gmail.com>
2019-04-08 15:57:13 -0400

    Ready to merge to develop

--- travelAngular/src/app/app.module.ts
+++ travelAngular/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
