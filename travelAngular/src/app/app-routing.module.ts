66cff88f1e1ee3e190cfd65dde4ce1d82688cbae
Laura Garcia <lgarcia223@gmail.com>
2019-04-11 13:47:14 -0400

    Tag Request Create page Working

--- travelAngular/src/app/app-routing.module.ts
+++ travelAngular/src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { RegisterComponent } from './Components/register/register.component';
 import { LoginComponent } from './Components/login/login.component';
 import { TagIndexComponent } from './Components/tag/tag-index/tag-index.component';
 import { TagRequestIndexComponent } from './Components/tagRequest/tag-request-index/tag-request-index.component';
+import { HomeComponent } from './Components/home/home.component';
+import { TagRequestCreateComponent } from './Components/tagRequest/tag-request-create/tag-request-create.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
@@ -16,7 +18,8 @@ const appRoutes: Routes = [
   { path: 'Register', component: RegisterComponent },
   { path: 'Login', component: LoginComponent },
   { path: 'tags' , component: TagIndexComponent},
-  { path: 'tagRequests' , component: TagRequestIndexComponent}
+  { path: 'tagRequests', component: TagRequestIndexComponent },
+  { path: 'tagRequests/create', component: TagRequestCreateComponent },
 ];
 
 @NgModule({
